272bbd0497f3fd7f32f89a1cc103aac5953015c4
paunicole <nicolecardozogomez@gmail.com>
2023-09-24 20:31:45 -0300

    Trae los mensajes de un canal

--- scripts/channel.js
+++ scripts/channel.js
@@ -1,28 +1,12 @@
 const urlParams = new URLSearchParams(window.location.search);
-const categoryID = urlParams.get("server_id");
+const serverID = urlParams.get("server_id");
 
-let taskBox = document.querySelector(".taskBox");
-let channelBtn = document.querySelector(".addTask");
+console.log("holaaa channel")
 
-// let catchTasks = (categoryID) => {
-//     let url = `http://127.0.0.1:5000/channels/?server_id=${categoryID}`;
-//     fetch(url)
-//     .then(res => res.json())
-//     .then(data => {
-//         data.map(channel => {
-//             createTaskContainer(channel)
-//         });
+let channelBox = document.querySelector(".channelBox");
 
-//         let addTaskBtn = document.querySelector(".addTask");
-//         addTaskBtn.addEventListener("click", () => {
-//             addTask(categoryID);
-//         });
-//     })
-//     .catch(err => console.log(err));
-// };
-
-let catchTasks = (categoryID) => {
-    let url = `http://127.0.0.1:5000/channels/?server_id=${categoryID}`;
+let catchChannels = (serverID) => {
+    let url = `http://127.0.0.1:5000/channels/?server_id=${serverID}`;
     fetch(url)
     .then(res => res.json())
     .then(data => {
@@ -30,80 +14,38 @@ let catchTasks = (categoryID) => {
             // Si no hay canales, muestra un mensaje de advertencia
             const noChannelsMessage = document.createElement("p");
             noChannelsMessage.textContent = "Aún no hay canales, crea uno.";
-            taskBox.appendChild(noChannelsMessage);
+            channelBox.appendChild(noChannelsMessage);
         } else {
             // Si hay canales, crea contenedores para cada uno
             data.map(channel => {
-                createTaskContainer(channel);
+                createChannelContainer(channel);
             });
         }
-
-        let addTaskBtn = document.querySelector(".addTask");
-        addTaskBtn.addEventListener("click", () => {
-            addTask(categoryID);
-        });
     })
+
     .catch(err => console.log(err));
 };
 
-let createTaskContainer = (channel) =>{
+let createChannelContainer = (channel) =>{
 
-    // Crear elemento de texto de servidor
-    let taskText = document.createElement("p");    // Elemento de texto para representar el nombre del canal
-    taskText.classList.add("channelText");            // Agrega la clase "channelText" a este elemento
-    //taskText.textContent = channel.name;           // Establece su contenido de texto con el nombre de la tarea obtenido de la variable tarea.name.
-    let canal_hashtag = `#${channel.name}`;
-    taskText.textContent = canal_hashtag;
-    console.log(canal_hashtag)
-    taskText.setAttribute("href", `home.html?server_id=${categoryID}/channel_id=${channel.channel_id}`);
-    taskText.setAttribute("data-channel-id", channel.channel_id);
+    // Crear elemento de texto para el nombre del canal
+    let channelText = document.createElement("a");
+    channelText.classList.add("channelText");
 
-    // Crear descripcion
-    //let dueTaskSpan = document.createElement("span");
-    //let dueTask = `Descripción: ${channel.descripcion}`;
-    //dueTaskSpan.textContent = dueTask;
-    //dueTaskSpan.classList.add("dueTaskSpan");
+    channelText.setAttribute("href", `home.html?server_id=${serverID}&channel_id=${channel.channel_id}`);
+    channelText.setAttribute("data-channel-id", channel.channel_id);
+
+    let channel_hashtag = `#${channel.name}`;
+    channelText.textContent = channel_hashtag;
 
     // Añadir elementos
-    taskText.appendChild(document.createElement("br"));
-    //taskText.appendChild(dueTaskSpan);
-    taskBox.appendChild(taskText);
-    taskText.setAttribute("data-channel-id", channel.channel_id);
-}
-
-// Función para crear un canal mediante una solicitud POST a una dirección URL local: "http://127.0.0.1:5000/channels/
-let addTask = (categoryID) => {
-    let taskName = prompt("Ingrese el nombre del canal");
-    let taskDescription = prompt("Ingrese la descripción del canal");
-
-    fetch("http://127.0.0.1:5000/channels/", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            name: taskName,
-            due_date: taskDescription,
-            category_id: +categoryID
-        })
-    })
-    .then(response => response.json())
-    .then(data => {
-        console.log("Canal creado:", data);
-    })
-    .catch(error => {
-        console.error("Error al crear el canal:", error);
-        alert("El nombre del canal y la fecha no puede estar vacío")
-    });
+    channelText.appendChild(document.createElement("br"));
+    channelBox.appendChild(channelText);
 }
 
 // CONTROL
-if (categoryID) {
-    catchTasks(categoryID);
+if (serverID) {
+    catchChannels(serverID);
 } else {
     console.log("No se ha proporcionado un ID de servidor.");
-    
 }
-
-
-
--- scripts/chat.js
+++ scripts/chat.js
@@ -1,88 +1,46 @@
-const urlParams = new URLSearchParams(window.location.search);
-const channelID = urlParams.get("channel_id");
+const urlParams2 = new URLSearchParams(window.location.search);
+const channelID = urlParams2.get("channel_id");
 
-let taskBox = document.querySelector(".chatBox");
-let channelBtn = document.querySelector(".addTask");
+let chatBox = document.querySelector(".chatBox");
 
-let catchChat = (channelID) => {
+console.log("holaaa chat")
+
+let catchChats = (channelID) => {
     let url = `http://127.0.0.1:5000/messages/?channel_id=${channelID}`;
     fetch(url)
     .then(res => res.json())
     .then(data => {
         if (data.length === 0) {
-            // Si no hay canales, muestra un mensaje de advertencia
-            const noChannelsMessage = document.createElement("p");
-            noChannelsMessage.textContent = "No hay mensajes. Escribe uno...";
-            taskBox.appendChild(noChannelsMessage);
+            // Si no hay mensajes, muestra un mensaje de advertencia
+            const noMessagesMessage = document.createElement("p");
+            noMessagesMessage.textContent = "No hay mensajes. Escribe uno...";
+            chatBox.appendChild(noMessagesMessage);
         } else {
-            // Si hay canales, crea contenedores para cada uno
+            // Si hay mensajes, crea contenedores para cada uno
             data.map(chat => {
-                createTaskContainer(chat);
+                createChatContainer(chat);
             });
         }
     })
     .catch(err => console.log(err));
 };
 
-catchChat();
-
-let createTaskContainer = (chat) =>{
+let createChatContainer = (chat) =>{
 
     // Crear elemento de texto de servidor
-    let taskText = document.createElement("p");   // Elemento de texto para representar el nombre del canal
-    taskText.classList.add("channelText");        // Agrega la clase "channelText" a este elemento
-    taskText.textContent = chat.message;          // Establece su contenido de texto con el nombre de la tarea obtenido de la variable chat.message
-
-    // Crear descripcion
-    //let dueTaskSpan = document.createElement("span");
-    //let dueTask = `Descripción: ${channel.descripcion}`;
-    //dueTaskSpan.textContent = dueTask;
-    //dueTaskSpan.classList.add("dueTaskSpan");
+    let chatText = document.createElement("p");   // Elemento de texto para representar el nombre del canal
+    chatText.classList.add("chatText");        // Agrega la clase "messageText" a este elemento
+    chatText.textContent = chat.message;          // Establece su contenido de texto con el nombre de la tarea obtenido de la variable chat.message
 
     // Añadir elementos
-    taskText.appendChild(document.createElement("br"));
-    //taskText.appendChild(dueTaskSpan);
-    taskBox.appendChild(taskText);
-    taskText.setAttribute("data-task-id", channel.channel_id);
-}
-
-// Función para crear un canal mediante una solicitud POST a una dirección URL local: "http://127.0.0.1:5000/channels/
-let addTask = (channelID) => {
-    let taskName = prompt("Ingrese el nombre del canal");
-    let taskDescription = prompt("Ingrese la descripción del canal");
-
-    fetch("http://127.0.0.1:5000/channels/", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            name: taskName,
-            due_date: taskDescription,
-            category_id: +categoryID
-        })
-    })
-    .then(response => response.json())
-    .then(data => {
-        console.log("Canal creado:", data);
-    })
-    .catch(error => {
-        console.error("Error al crear el canal:", error);
-        alert("El nombre del canal y la fecha no puede estar vacío")
-    });
+    chatText.appendChild(document.createElement("br"));
+    chatBox.appendChild(chatText);
+    chatText.setAttribute("data-chat-id", chat.message_id);
 }
 
 // CONTROL
 if (channelID) {
-    catchChat(channelID);
+    catchChats(channelID);
 } else {
     console.log("No se ha proporcionado un ID de canal.");
 }
-
-document.getElementById('chatBox').addEventListener('click', event => {
-    if (event.target.classList.contains('chatBox')) {
-      const canalSeleccionado = event.target.dataset.canalId; // Hay que definir 'data-canal-id' en los elementos de canal
-      
-      console.log(canalSeleccionado);
-    }
-  });
